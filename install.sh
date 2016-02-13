4683dbb658ce93c9709df54c92c73eb8bd01556a
Philippe Loctaux <phil@philippeloctaux.com>
2016-02-13 01:29:16 +0100

    security shit

--- install.sh
+++ install.sh
@@ -6,10 +6,14 @@ echo
 
 echo "Copying the configuration file for msmtprc"
 cp ./dot-msmtprc ~/.msmtprc
+chmod 600 ~/.msmtprc
 
 echo "Copying the configuration files for mutt"
 cp ./dot-mutt ~/.mutt -r
+chmod 700 ~/.mutt/
+chmod 600 ~/.mutt/*
 cp ./dot-muttrc ~/.muttrc
+chmod 600 ~/.muttrc
 
 echo "Now edit the files for the passwords"
 echo "Look at app passwords for gmail"
