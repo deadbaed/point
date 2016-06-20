bcad5b9502bc69f2d8966efa37db7ed502d879d0
Philippe Loctaux <phil@philippeloctaux.com>
2016-06-20 20:52:22 +0200

    updated git credential helper

--- install.sh
+++ install.sh
@@ -3,4 +3,17 @@
 
 echo "copying gitconfig file"
 cp ./gitconfig ~/.gitconfig
+
+echo "to get git credential helper"
+echo ""
+echo "ubuntu"
+echo "1. sudo apt install libgnome-keyring-dev"
+echo "2. sudo make --directory=/usr/share/doc/git/contrib/credential/gnome-keyring"
+echo "3. git config --global credential.helper /usr/share/doc/git/contrib/credential/gnome-keyring/git-credential-gnome-keyring"
+echo ""
+echo "arch"
+echo "1. sudo pacman -S libgnome-keyring"
+echo "2. cd /usr/share/git/credential/gnome-keyring && sudo make"
+echo "3. git config --global credential.helper /usr/lib/git-core/git-credential-gnome-keyring"
+
 echo "done"
