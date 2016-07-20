45aaf8c2b801489c38e30dac0c97eddc5c73e1a6
Philippe Loctaux <phil@philippeloctaux.com>
2016-07-20 20:22:30 +0200

    added remove function & fixes

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
-echo "install zsh and ohmyzsh (cause ohmyzsh is <3)"
-cp ./my-zsh/ ~/.my-zsh -r
-ln -s ~/.my-zsh/zsh-config .zshrc;
-echo "done"
+echo "removing old zsh config";
+rm -rf ~/.my-zsh/ ~/.zshrc;
+
+echo "install zsh and ohmyzsh (cause ohmyzsh is <3)";
+cp ./my-zsh/ ~/.my-zsh -r;
+ln -s ~/.my-zsh/zsh-config ~/.zshrc;
+source ~/.zshrc;
+echo "done";
