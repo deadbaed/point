5693ea2a677a33527ffdb6c870aee6f9161920be
Philippe Loctaux <phil@philippeloctaux.com>
2016-07-23 16:24:13 +0200

    fixed install script

--- install.sh
+++ install.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
+FOLDER=$1
+
+if cd $FOLDER; then
+	echo "copying config file";
+	cp config $FOLDER;
+else
+	echo "gimme a folder to put the config file plz"
+	exit 1;
+fi
+
+if [ "$FOLDER" == "" ] ; then
+	echo "gimme a folder to put the config file plz"
+	exit 1;
+fi
 
 echo "dependecies used"
 echo "pactl feh"
-
-run "install.sh $1"
-$1 is the path of the i3 config directory
-
 
