47f28af52532f5cc0795f9b31de7c6043163dee7
Philippe Loctaux <phil@philippeloctaux.com>
2016-07-31 14:38:14 +0200

    various changes

--- install.sh
+++ install.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 #
 
-echo "creating gpg foler & copying gpg config file"
-mkdir ~/.gnupg
-cp ./gpg.conf ~/.gnupg/gpg.conf -v
-cp ./gpg-agent.conf ~/.gnupg/gpg-agent.conf -v
+cp ./gpg.conf ~/.gnupg/gpg.conf
+cp ./gpg-agent.conf ~/.gnupg/gpg-agent.conf
+chmod 600 ~/.gnupg/*;
 echo "done"
