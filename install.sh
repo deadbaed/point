77d667bd67f90724439e85b788d3513d84e10ee2
Philippe Loctaux <phil@philippeloctaux.com>
2016-08-23 17:55:44 +0200

    added dep

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 echo "removing old zsh config";
 rm -rf ~/.my-zsh/ ~/.zshrc;
 
-echo "install zsh and ohmyzsh (cause ohmyzsh is <3)";
+echo "install zsh , ruby, git, and ohmyzsh (cause ohmyzsh is <3)";
 cp -r ./my-zsh/ ~/.my-zsh;
 ln -s ~/.my-zsh/zsh-config ~/.zshrc;
 sleep 2;source ~/.zshrc;source ~/.zshrc;
