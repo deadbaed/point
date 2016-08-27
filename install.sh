4b05fca06d0360ee7c7b3f30166d332294005564
Philippe Loctaux <phil@philippeloctaux.com>
2016-08-27 22:20:45 +0200

    added systemd thing

--- install.sh
+++ install.sh
@@ -5,4 +5,8 @@ cp ./gpg.conf ~/.gnupg/gpg.conf;
 cp ./gpg-agent.conf ~/.gnupg/gpg-agent.conf;
 chmod 700 ~/.gnupg/;
 chmod 600 ~/.gnupg/*;
+
+mkdir -p ~/.config/systemd/user/;
+cp ./systemd-thing ~/.config/systemd/user/gpg-agent.service;
+
 echo "done";
