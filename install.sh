9def376fd08f25a3c4fafcdedf3b7bcdb674186a
Philippe Loctaux <loctauxphilippe@gmail.com>
2017-12-12 11:52:20 +0100

    fuck that

--- install.sh
+++ install.sh
@@ -11,10 +11,8 @@ cp ./vimrc ~/.vim/vimrc
 ln -s ~/.vim/vimrc ~/.vimrc;
 echo "config copied and linked"
 
-mkdir -p ~/.vim/autoload
-wget https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-#curl -sfLo ~/.vim/autoload/plug.vim --create-dirs \
-#https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -sfLo ~/.vim/autoload/plug.vim --create-dirs \
+https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 echo "vim plug installed"
 
 echo "bye"
