5ce2bced3da8e4cb3575edd0942806c0492d8ea1
Philippe Loctaux <loctauxphilippe@gmail.com>
2018-01-02 14:11:48 +0100

    echo for jekyll

--- install.sh
+++ install.sh
@@ -5,5 +5,6 @@ echo "copying new zsh config";
 cp -r ./my-zsh/ ~/.my-zsh;
 ln -s ~/.my-zsh/zsh-config ~/.zshrc;
 
+echo "install ruby also for jekyll" ;
 echo "please run 'source ~/.zshrc;' in order to get the config working";
 echo "done";
