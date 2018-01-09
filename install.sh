1862771a7e661ffbc6aca2641c9e22963da3a29c
Philippe Loctaux <loctauxphilippe@gmail.com>
2018-01-09 21:32:17 +0100

    using offlineimap now

--- install.sh
+++ install.sh
@@ -1,8 +1,14 @@
 # install.sh
 #
 
-echo "Install packages 'mutt', 'msmtp', 'w3m' and 'eog'"
+echo "Install packages 'mutt', 'msmtp', 'offlineimap', 'w3m' and 'eog'"
 echo
+
+echo "Copying the configuration file for offlineimap"
+cp ./offlineimap-rc ~/.offlineimaprc
+chmod 600 ~/.offlineimaprc
+mkdir -p ~/email/gmail
+chmod 700 ~/email -R
 
 echo "Copying the configuration file for msmtprc"
 cp ./msmtprc ~/.msmtprc
