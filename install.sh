6c3b609ec3f42ab21949f1967179da055f3a8699
Philippe Loctaux <loctauxphilippe@gmail.com>
2018-01-21 20:46:20 +0100

    vim plug install

--- install.sh
+++ install.sh
@@ -15,5 +15,6 @@ echo "config copied and linked"
 curl -sfLo ~/.vim/autoload/plug.vim --create-dirs \
 https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 echo "vim plug installed"
+echo "run :PlugInstall to install all the plugins to vim"
 
 echo "bye"
