b447b7df4b1ccf16e6c0dfecc1f3e0c13675a135
Philippe Loctaux <loctauxphilippe@gmail.com>
2018-02-05 04:51:19 +0100

    new files
    i spent a lot of time fixing that issue

--- git/install.sh
+++ git/install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #
 
-ln -s ~/.gitconfig ./gitconfig
-ln -s ~/.gitignore_global ./gitignore_global
-ln -s ~/.git_template ./git_template/
+ln -s ~/point/git/.gitconfig ~/
+ln -s ~/point/git/.gitignore ~/
+ln -s ~/point/git/.git_template ~/
 
 echo "install msmtp lolcommits"
 echo "make sure of the login username "
