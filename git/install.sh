7639149920538831162300be333d117487f884f1
Philippe Loctaux <loctauxphilippe@gmail.com>
2018-02-05 05:15:35 +0100

    removing old git config

--- git/install.sh
+++ git/install.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 #
 
+rm -rf ~/.gitconfig ~/.gitignore ~/.git_template
+echo "removed old git config"
+echo ""
+
 ln -s ~/point/git/.gitconfig ~/
 ln -s ~/point/git/.gitignore ~/
 ln -s ~/point/git/.git_template ~/
