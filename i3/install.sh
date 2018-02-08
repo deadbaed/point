36b986948ba269ce4f2e275b9162373d41935f5b
Philippe Loctaux <loctauxphilippe@gmail.com>
2018-02-08 09:16:23 +0100

    i3: linking instead of copying

--- i3/install.sh
+++ i3/install.sh
@@ -1,21 +1,10 @@
 #!/bin/bash
 #
 
-FOLDER=$1
+ln -s ~/point/i3/config ~/.config/i3/config
 
-if [ "$FOLDER" == "" ] ; then
-	echo "gimme a folder to put the config file plz"
-	exit 1;
-fi
-
-if cd $FOLDER; then
-	echo "copying config file";
-	cp config $FOLDER;
-else
-	echo "gimme a folder to put the config file plz"
-	exit 1;
-fi
-
+echo "config ready";
+echo "restart i3";
 echo "";
-echo "dependecies used";
-echo "pactl feh rofi light-git(aur) imgur-screenshot-git(aur)";
+echo "you need to install:";
+echo "i3 termite gnome-keyring libsecret pactl feh rofi light-git(aur) imgur-screenshot-git(aur)";
