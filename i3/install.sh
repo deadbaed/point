d0899ee1c59e07c4a91c6b8566bdadfe6c2ceb13
Philippe Loctaux <loctauxphilippe@gmail.com>
2018-02-09 16:03:17 +0100

    proper list of requirements

--- i3/install.sh
+++ i3/install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #
 
+echo "removing old config";
 rm ~/.config/i3/config
 
 ln -s ~/point/i3/config ~/.config/i3/config
@@ -8,5 +9,4 @@ ln -s ~/point/i3/config ~/.config/i3/config
 echo "config ready";
 echo "restart i3 (*mod + shift + r* if you're just updating the config)";
 echo "";
-echo "you need to install:";
-echo "i3 xfce4-terminal network-manager-applet gnome-keyring libsecret pactl feh rofi light-git(aur) imgur-screenshot-git(aur)";
+echo "check the file *requirements.txt* to see what to install";
