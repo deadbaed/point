fa7ed3689e0f40311c3e9fab35e3c244be7042e4
Philippe Loctaux <loctauxphilippe@gmail.com>
2018-02-09 15:35:41 +0100

    i3: removing old config and little message if updating the config

--- i3/install.sh
+++ i3/install.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 #
 
+rm ~/.config/i3/config
+
 ln -s ~/point/i3/config ~/.config/i3/config
 
 echo "config ready";
-echo "restart i3";
+echo "restart i3 (*mod + shift + r* if you're just updating the config)";
 echo "";
 echo "you need to install:";
 echo "i3 xfce4-terminal network-manager-applet gnome-keyring libsecret pactl feh rofi light-git(aur) imgur-screenshot-git(aur)";
