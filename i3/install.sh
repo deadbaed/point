ac4f86e8d8be91b8546b5e447148e0ec74b128bc
Philippe Loctaux <loctauxphilippe@gmail.com>
2018-02-09 16:18:31 +0100

    i3: wallpaper location is ~/.wallpaper.jpg now

--- i3/install.sh
+++ i3/install.sh
@@ -9,4 +9,5 @@ ln -s ~/point/i3/config ~/.config/i3/config
 echo "config ready";
 echo "restart i3 (*mod + shift + r* if you're just updating the config)";
 echo "";
+echo "wallpaper is at ~/.wallpaper.jpg";
 echo "check the file *requirements.txt* to see what to install";
