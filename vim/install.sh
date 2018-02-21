8e04c64149f29e19ee1583a97a6bce520997349b
Philippe Loctaux <loctauxphilippe@gmail.com>
2018-02-21 09:44:47 +0100

    vim: install.sh: moved vim-plug installation into vimrc

--- vim/install.sh
+++ vim/install.sh
@@ -3,13 +3,10 @@
 rm -rf ~/.vimrc ~/.vim/
 echo "removing old vim config and plugins"
 
-mkdir -p ~/.vim/ ~/.vim/backup/ ~/.vim/swap/ ~/.vim/undo/
+mkdir -p ~/.vim/ ~/.vim/backup/ ~/.vim/swap/ ~/.vim/undo/ ~/.vim/autoload/
 ln -s ~/point/vim/vimrc ~/.vimrc
 echo "config ready" 
 
-curl -sfLo ~/.vim/autoload/plug.vim --create-dirs \
-https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-echo "vim plug installed"
-echo "run :PlugInstall to install all the plugins to vim"
+echo "dont forget to run :PlugInstall to install all the plugins to vim"
 
 echo "bye"
