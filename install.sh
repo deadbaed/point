d904328ea0e2092d784d0231893a28d3486bcf0e
Philippe Loctaux <loctauxphilippe@gmail.com>
2018-03-10 00:29:04 +0100

    script tells you which files to edit after copying them

--- install.sh
+++ install.sh
@@ -4,19 +4,19 @@
 echo "Install packages 'mutt', 'msmtp', 'offlineimap', 'w3m' and 'eog'"
 echo
 
-echo "Copying the configuration file for offlineimap"
+echo "edit ~/.offlineimaprc"
 cp ./offlineimap-rc ~/.offlineimaprc
 chmod 600 ~/.offlineimaprc
 mkdir -p ~/email/gmail
 chmod 700 ~/email -R
 
-echo "Copying the configuration file for msmtprc"
+echo "edit ~/.msmtprc"
 cp ./msmtprc ~/.msmtprc
 chmod 600 ~/.msmtprc
 
-echo "Copying the configuration files for mutt"
+echo "edit ~/.muttrc"
 cp ./mutt-rc ~/.muttrc
 cp ./mutt-mailcap ~/.mutt-mailcap
 chmod 600 ~/.muttrc
 
-echo "Now edit the files for the passwords"
+echo "done"
