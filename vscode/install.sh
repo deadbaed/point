a074423f3d0b0e57491be27648ad17a83e905f48
Philippe Loctaux <loctauxphilippe@gmail.com>
2018-04-10 00:34:17 +0200

    vscode: works

--- vscode/install.sh
+++ vscode/install.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 #
 
+# make the directory in case it doesnt exist yet
+mkdir -p ~/.config/Code/User/
+
+# link the settings file
+ln -s ~/point/vscode/settings.json ~/.config/Code/User
+
+echo "done"
