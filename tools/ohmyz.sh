932b2bd9251727c631665b546850b9ea0f454614
Philippe Loctaux <loctauxphilippe@gmail.com>
2018-07-06 01:10:52 +0200

    tools: first script, ohmyzsh

---
+++ tools/ohmyz.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+
+echo "https://ohmyz.sh"
+
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+echo "log out and back in, and install zsh point"
