8de255353b189c5aa308cf060cc92910a7f86d86
Philippe Loctaux <loctauxphilippe@gmail.com>
2018-08-13 16:29:36 +0200

    email: added epitech to offlineimap

--- email/offlineimap.py
+++ email/offlineimap.py
@@ -3,3 +3,5 @@ from subprocess import check_output
 
 def pass_gmail():
     return check_output("pass google/loctauxphilippe/email-app-password", shell=True).splitlines()[0]
+def pass_epitech():
+    return check_output("pass epitech/password", shell=True).splitlines()[0]
