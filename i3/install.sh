605f4d1c6ac9b19ea51f9530ae23e8481efe91c0
Philippe Loctaux <p@philippeloctaux.com>
2018-08-27 23:07:43 +0200

    i3: added i3status config

--- i3/install.sh
+++ i3/install.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 #
 
+# i3 config
 mkdir -p ~/.config/i3/
+ln -s ~/point/i3/i3-config ~/.config/i3/config
 
-echo "removing old config";
-rm ~/.config/i3/config
+# i3status config
+mkdir -p ~/.config/i3status/
+ln -s ~/point/i3/i3status-config ~/.config/i3status/config
 
-ln -s ~/point/i3/config ~/.config/i3/config
 
 echo "config ready";
-echo "restart i3 (*mod + shift + r* if you're just updating the config)";
-echo "";
+echo;
+echo "restart i3 (*mod + shift + c* if you're just updating the config)";
+echo;
 echo "wallpaper is at ~/.wallpaper.jpg";
 echo "check the file *requirements.txt* to see what to install (most is for arch)";
