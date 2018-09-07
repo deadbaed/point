77469a1523341bef51046e09575377b90c733ac1
Philippe Loctaux <p@philippeloctaux.com>
2018-09-07 17:30:59 +0200

    git: updated install script

--- git/install.sh
+++ git/install.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 #
 
-rm -rf ~/.gitconfig ~/.gitignore ~/.git_template
+rm -rf ~/.gitconfig ~/.gitignore
 echo "removed old git config"
 echo ""
 
 ln -s ~/point/git/.gitconfig ~/
 ln -s ~/point/git/.gitignore ~/
-ln -s ~/point/git/.git_template ~/
 
 echo "install msmtp lolcommits"
 echo "make sure of the login username "
