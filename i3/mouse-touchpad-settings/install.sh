ec37334712768888b5bc58575df60f28da18fa0d
Philippe Loctaux <p@philippeloctaux.com>
2018-09-08 14:26:11 +0200

    mouse-touchpad: added mouse settings

--- i3/mouse-touchpad-settings/install.sh
+++ i3/mouse-touchpad-settings/install.sh
@@ -1,4 +1,5 @@
 sudo pacman -S libinput xf86-input-libinput xorg-xinput
-echo "copying the configuration file to '/etc/X11/xorg.conf.d/'";
+echo "copying configuration files to '/etc/X11/xorg.conf.d/'";
 sudo cp 30-touchpad.conf /etc/X11/xorg.conf.d/30-touchpad.conf;
+sudo cp 80-any-pointer-mouse.conf /etc/X11/xorg.conf.d/80-any-pointer-mouse.conf;
 echo "done";
