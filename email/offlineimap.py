97d647e6a0fa69a7dc5c2fffff4dc8d83c7823bf
Philippe Loctaux <p@philippeloctaux.com>
2019-04-09 13:25:05 +0200

    email: updated pass for gmail

--- email/offlineimap.py
+++ email/offlineimap.py
@@ -2,7 +2,7 @@
 from subprocess import check_output
 
 def pass_gmail():
-    return check_output("pass google/loctauxphilippe/email-app-password", shell=True).splitlines()[0]
+    return check_output("pass google.com/loctauxphilippe@gmail.com/email-app-password", shell=True).splitlines()[0]
 def pass_epitech():
     return check_output("pass epitech/philippe.loctaux", shell=True).splitlines()[0]
 def pass_plcom():
