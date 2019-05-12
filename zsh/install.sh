8155b5f616135413884970c2469fd8f1ec3e7274
Philippe Loctaux <p@philippeloctaux.com>
2019-05-12 17:57:35 +0200

    zsh: updated install script

--- zsh/install.sh
+++ zsh/install.sh
@@ -2,12 +2,13 @@ echo "INSTALL HTTP://OHMYZ.SH";
 
 echo "removing old zsh config";
 rm -rf ~/.zshrc;
+rm -rf ~/.zshenv;
 
 echo "setting up new zsh config";
 ln -s ~/point/zsh/zshrc ~/.zshrc;
 ln -s ~/point/zsh/zshenv ~/.zshenv;
 
-mkdir ~/.gems # for gems
+mkdir ~/.gems -p # for gems
 
 echo "please run 'source ~/.zshrc;' in order to get the config working";
 echo "done";
