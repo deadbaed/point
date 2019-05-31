d66fdb1edc247225111643b3eea5324c8f68394c
Philippe Loctaux <p@philippeloctaux.com>
2019-05-31 17:32:14 +0200

    dwm: added requirements to readme

--- dwm/config.h
+++ dwm/config.h
@@ -63,7 +63,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *roficmd[] = { "rofi", "-combi-modi", "drun,run,window,ssh", "-show", "combi", NULL };
+static const char *roficmd[] = { "rofi", "-combi-modi", "drun,run", "-show", "combi", NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *lockscreencmd[] = { "xscreensaver-command", "-lock", NULL };
 
@@ -77,6 +77,7 @@ static const char *brightness_up[] = { "xbacklight", "-inc", "05", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
+	{ MODKEY|ShiftMask,             XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_p,      spawn,          {.v = roficmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
