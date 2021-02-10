01a79bcd28f9d392883eb5ca2a1d18482b092f70
Philippe Loctaux <p@philippeloctaux.com>
2021-02-10 22:51:30 +0100

    git: removed gitignore and post-commit lolcommits hook

--- git/install.sh
+++ git/install.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 #
 
-rm -rf ~/.gitconfig ~/.gitignore
+rm -f ~/.gitconfig
 echo "removed old git config"
-echo ""
 
 ln -s ~/point/git/gitconfig ~/.gitconfig
-ln -s ~/point/git/gitignore ~/.gitignore
-cp ./git_template ~/.git_template -r
 
-echo "install msmtp lolcommits"
-echo "make sure of the login username "
-echo "check the pgp key"
 echo ""
 echo "done"
