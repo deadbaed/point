4166141a5ca7a986f34a4145e526f8dd2a1cc27e
Philippe Loctaux <p@philippeloctaux.com>
2021-06-04 08:17:09 +0200

    added gvimrc

--- vim/install.sh
+++ vim/install.sh
@@ -5,6 +5,7 @@ echo "removing old vim config and plugins"
 
 mkdir -p ~/.vim/ ~/.vim/backup/ ~/.vim/swap/ ~/.vim/undo/ ~/.vim/autoload/
 ln -s ~/point/vim/vimrc ~/.vimrc
+ln -s ~/point/vim/gvimrc ~/.gvimrc
 echo "config ready"
 
 echo "dont forget to run :PlugInstall to install all the plugins to vim"
