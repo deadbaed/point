4fa3652cf4bcadb9705c0d96c91945370eb871d2
Philippe Loctaux <p@philippeloctaux.com>
2021-07-14 13:10:14 +0200

    added ideavimrc

--- vim/install.sh
+++ vim/install.sh
@@ -6,6 +6,7 @@ echo "removing old vim config and plugins"
 mkdir -p ~/.vim/ ~/.vim/backup/ ~/.vim/swap/ ~/.vim/undo/ ~/.vim/autoload/
 ln -s ~/point/vim/vimrc ~/.vimrc
 ln -s ~/point/vim/gvimrc ~/.gvimrc
+ln -s ~/point/vim/ideavimrc ~/.ideavimrc
 echo "config ready"
 
 echo "dont forget to run :PlugInstall to install all the plugins to vim"
