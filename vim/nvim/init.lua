5c768077992dec734cdb478c33b95ff57d5d5425
Philippe Loctaux <p@philippeloctaux.com>
2024-01-18 17:53:16 +0100

    neovim: fuzzy finder file browser, wip "which-key"

--- vim/nvim/init.lua
+++ vim/nvim/init.lua
@@ -140,7 +140,25 @@ require("lazy").setup({
         }
       })
     end
-  }
+  },
+
+  -- fuzzy finder file browser
+  {
+    "nvim-telescope/telescope-file-browser.nvim",
+    dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" }
+  },
+
+  -- helper to show which key is mapped to what
+  -- TODO: make it work with existing bindings
+  -- {
+  --   "folke/which-key.nvim",
+  --   event = "VeryLazy",
+  --   init = function()
+  --     vim.o.timeout = true
+  --     vim.o.timeoutlen = 300
+  --   end,
+  --   opts = {}
+  -- }
 })
 
 -- update time for git status in files
@@ -177,11 +195,20 @@ require("ibl").setup {
 
 -- telescope
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-vim.keymap.set('n', '<leader>fr', builtin.lsp_references, {})
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {desc="Find files"})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {desc="Find Grep"})
+vim.keymap.set('n', '<leader>fo', builtin.buffers, {desc="Find opened buffers"})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {desc="Find help tags"})
+vim.keymap.set('n', '<leader>fr', builtin.lsp_references, {desc="Find references"})
+
+-- telescope file browser
+require("telescope").load_extension "file_browser"
+vim.api.nvim_set_keymap(
+  "n",
+  "<leader>fb",
+  ":Telescope file_browser path=%:p:h select_buffer=true<CR>",
+  { noremap = true, desc = "File browser" }
+)
 
 -- line numbers
 vim.wo.number = true
