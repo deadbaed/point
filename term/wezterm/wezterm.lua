8400a2d8304c8bc260d6f96464bd9a61a84eab21
Philippe Loctaux <p@philippeloctaux.com>
2024-01-19 21:18:16 +0100

    wezterm: formatting, fontsize=13 on macos

--- term/wezterm/wezterm.lua
+++ term/wezterm/wezterm.lua
@@ -1,5 +1,5 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require "wezterm"
 
 -- This table will hold the configuration.
 local config = {}
@@ -10,12 +10,28 @@ if wezterm.config_builder then
   config = wezterm.config_builder()
 end
 
-config.default_prog = { 'zsh', '--login', '--interactive', '-l', '-c', 'zellij' }
+config.default_prog = { "zsh", "--login", "--interactive", "-l", "-c", "zellij" }
 config.audible_bell = "Disabled"
 
--- font
-config.font = wezterm.font('JetBrains Mono', { weight = 'Medium' })
-config.font_size = 11
+-- source: https://gist.github.com/Zbizu/43df621b3cd0dc460a76f7fe5aa87f30
+function GetOS()
+  local osname
+
+  -- unix systems
+  local fh = assert(io.popen("uname -o 2>/dev/null", "r"))
+  if fh then
+    osname = fh:read()
+  end
+
+  return osname or "Windows"
+end
+
+-- font size
+local font_size = 11
+if GetOS() == "Darwin" then
+  font_size = 13
+end
+config.font_size = font_size
 
 -- window
 config.hide_tab_bar_if_only_one_tab = true
@@ -28,10 +44,10 @@ config.window_padding = {
 
 -- use alt <- -> to go backwards / forwards of a word
 config.keys = {
-    -- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
-    {key="LeftArrow", mods="OPT", action=wezterm.action{SendString="\x1bb"}},
-    -- Make Option-Right equivalent to Alt-f; forward-word
-    {key="RightArrow", mods="OPT", action=wezterm.action{SendString="\x1bf"}},
+  -- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
+  { key = "LeftArrow",  mods = "OPT", action = wezterm.action { SendString = "\x1bb" } },
+  -- Make Option-Right equivalent to Alt-f; forward-word
+  { key = "RightArrow", mods = "OPT", action = wezterm.action { SendString = "\x1bf" } },
 }
 
 return config
