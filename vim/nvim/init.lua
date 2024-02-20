928cf98f983e7ac05033ec58376d079a5d124385
Philippe Loctaux <p@philippeloctaux.com>
2024-02-20 16:11:06 +0100

    neovim: telescope: show hidden files

--- vim/nvim/init.lua
+++ vim/nvim/init.lua
@@ -369,6 +369,13 @@ require("mini.indentscope").setup({
 })
 
 -- telescope
+require("telescope").setup {
+  pickers = {
+    find_files = {
+      hidden = true
+    }
+  }
+}
 local builtin = require("telescope.builtin")
 vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Find Grep" })
