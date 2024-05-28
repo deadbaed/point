294e64ecba13050b3f802cfc30cb64e5b5aba0ec
Philippe Loctaux <p@philippeloctaux.com>
2024-05-28 17:10:43 +0200

    neovim: gitsigns: more keyboard shortcuts

--- vim/nvim/init.lua
+++ vim/nvim/init.lua
@@ -118,9 +118,23 @@ require("lazy").setup({
     "lewis6991/gitsigns.nvim",
     config = function()
       require("gitsigns").setup({
-        on_attach = function()
-          local gs = package.loaded.gitsigns
-          vim.keymap.set("n", "<leader>hp", gs.preview_hunk, { desc = "Preview hunk" })
+        on_attach = function(bufnr)
+          local gitsigns = require("gitsigns")
+
+          local function map(mode, l, r, opts)
+            opts = opts or {}
+            opts.buffer = bufnr
+            vim.keymap.set(mode, l, r, opts)
+          end
+
+          map("n", "<leader>hs", gitsigns.stage_hunk, { desc = "Hunk Stage" })
+          map("n", "<leader>hr", gitsigns.reset_hunk, { desc = "Hunk Reset" })
+          map("v", "<leader>hs", function() gitsigns.stage_hunk { vim.fn.line("."), vim.fn.line("v") } end,
+            { desc = "Visual Hunk Stage" })
+          map("v", "<leader>hr", function() gitsigns.reset_hunk { vim.fn.line("."), vim.fn.line("v") } end,
+            { desc = "Visual Hunk Reset" })
+          map("n", "<leader>hu", gitsigns.undo_stage_hunk, { desc = "Hunk Undo Stage" })
+          map("n", "<leader>hp", gitsigns.preview_hunk, { desc = "Hunk Preview" })
         end
       })
 
