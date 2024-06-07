fd919fc955717145c0e43e1cd97593d6a6b6b983
Philippe Loctaux <p@philippeloctaux.com>
2024-06-07 15:20:25 +0200

    neovim: markdown preview, requires `deno` to be installed

--- vim/nvim/init.lua
+++ vim/nvim/init.lua
@@ -563,7 +563,17 @@ require("lazy").setup({
   },
   { -- distraction free
     "folke/zen-mode.nvim",
-  }
+  },
+  { -- markdown preview
+    "toppair/peek.nvim",
+    event = { "VeryLazy" },
+    build = "deno task --quiet build:fast",
+    config = function()
+      require("peek").setup()
+      vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
+      vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
+    end,
+  },
 })
 
 -- update time for git status in files
