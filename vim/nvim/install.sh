5ccd733dd668c8e4453315ba22a7a565281221a1
Philippe Loctaux <p@philippeloctaux.com>
2024-12-17 13:45:46 +0100

    neovim: tracking lazy-lock.json, symlink to entire directory

--- vim/nvim/install.sh
+++ vim/nvim/install.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 #
 
-rm -rf $HOME/.config/nvim
-mkdir -p $HOME/.config/nvim
-ln -s $(realpath ./init.lua) $HOME/.config/nvim/init.lua
+rm -rf "$HOME/.config/nvim"
+ln -s "$(realpath .)" "$HOME/.config/nvim"
