8c528a56167e4a6a040c587408dfaf4bf03284be
Philippe Loctaux <p@philippeloctaux.com>
2024-12-23 14:19:58 +0100

    neovim: fixed in 0.10.3

--- vim/nvim/init.lua
+++ vim/nvim/init.lua
@@ -337,17 +337,6 @@ require("lazy").setup({
 
       local lspconfig = require("lspconfig")
 
-      -- FIXME: https://github.com/neovim/neovim/issues/30985
-      for _, method in ipairs({ "textDocument/diagnostic", "workspace/diagnostic" }) do
-        local default_diagnostic_handler = vim.lsp.handlers[method]
-        vim.lsp.handlers[method] = function(err, result, context, config)
-          if err ~= nil and err.code == -32802 then
-            return
-          end
-          return default_diagnostic_handler(err, result, context, config)
-        end
-      end
-
       -- configure lsp installed with mason
       require("mason").setup({})
       require("mason-lspconfig").setup({
