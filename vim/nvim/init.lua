ed3636798095b42f78f20cd4f7bfd02de5bd0c82
Philippe Loctaux <p@philippeloctaux.com>
2024-12-28 13:42:05 +0100

    neovim: oil: show hidden files by default

--- vim/nvim/init.lua
+++ vim/nvim/init.lua
@@ -134,7 +134,11 @@ require("lazy").setup({
     opts = {},
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
-      require("oil").setup()
+      require("oil").setup({
+        view_options = {
+          show_hidden = true,
+        },
+      })
       vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
     end
   },
