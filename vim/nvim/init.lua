6a6bea160c9f2e5cc40595d58b7b480d9577aed6
Philippe Loctaux <p@philippeloctaux.com>
2025-06-05 20:42:27 +0200

    neovim: plugin to discover vim motions

--- vim/nvim/init.lua
+++ vim/nvim/init.lua
@@ -106,6 +106,32 @@ require("lazy").setup({
         },
       },
     },
+    { -- discover vim motions
+      "tris203/precognition.nvim",
+      opts = {
+        startVisible = true,
+        showBlankVirtLine = true,
+        highlightColor = { link = "Comment" },
+        hints = {
+          Caret = { text = "^", prio = 2 },
+          Dollar = { text = "$", prio = 0 },
+          MatchingPair = { text = "%", prio = 0 },
+          Zero = { text = "0", prio = 0 },
+          w = { text = "w", prio = 10 },
+          b = { text = "b", prio = 9 },
+          e = { text = "e", prio = 8 },
+          W = { text = "W", prio = 7 },
+          B = { text = "B", prio = 6 },
+          E = { text = "E", prio = 5 },
+        },
+        gutterHints = {
+          G = { text = "G", prio = 0 },
+          gg = { text = "gg", prio = 0 },
+          PrevParagraph = { text = "{", prio = 8 },
+          NextParagraph = { text = "}", prio = 8 },
+        },
+      },
+    },
     { -- syntax highlight
       "nvim-treesitter/nvim-treesitter",
       build = ":TSUpdate",
