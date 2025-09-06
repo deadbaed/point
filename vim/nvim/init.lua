8a76650cff872d902eb5517e94ed43f706118f84
Philippe Loctaux <p@philippeloctaux.com>
2025-09-07 01:37:46 +0200

    neovim: vue_ls configuration got moved to lspconfig
    thanks! https://github.com/neovim/nvim-lspconfig/pull/3943

--- vim/nvim/init.lua
+++ vim/nvim/init.lua
@@ -833,35 +833,6 @@ vim.lsp.config("vtsls", {
   },
 })
 
--- lsp: vue
-vim.lsp.config("vue_ls", {
-  on_init = function(client)
-    client.handlers["tsserver/request"] = function(_, result, context)
-      local clients = vim.lsp.get_clients({ bufnr = context.bufnr, name = "vtsls" })
-      if #clients == 0 then
-        vim.notify("Could not find `vtsls` lsp client, `vue_ls` would not work without it.", vim.log.levels.ERROR)
-        return
-      end
-      local ts_client = clients[1]
-
-      local param = unpack(result)
-      local id, command, payload = unpack(param)
-      ts_client:exec_cmd({
-        title = "vue_request_forward", -- You can give title anything as it's used to represent a command in the UI, `:h Client:exec_cmd`
-        command = "typescript.tsserverRequest",
-        arguments = {
-          command,
-          payload,
-        },
-      }, { bufnr = context.bufnr }, function(_, r)
-        local response_data = { { id, r.body } }
-        ---@diagnostic disable-next-line: param-type-mismatch
-        client:notify("tsserver/response", response_data)
-      end)
-    end
-  end,
-})
-
 if vim.g.neovide then
   function GetOS()
     -- source: https://gist.github.com/Zbizu/43df621b3cd0dc460a76f7fe5aa87f30
