6ef1ba6c07069871c0255197560df99f357d259f
Philippe Loctaux <p@philippeloctaux.com>
2026-07-16 19:20:25 +0200

    helix.nix: tweaks

--- helix.nix
+++ helix.nix
@@ -3,7 +3,11 @@
   programs.helix = {
     enable = true;
     settings = {
+      # TODO: switch to dual theme when helix > 25.07 is out
       theme = "catppuccin_latte";
+      # theme.dark = "catppuccin_mocha"
+      # them.light = "catppuccin_latte"
+
       editor = {
         line-number = "relative";
         lsp = {
@@ -12,6 +16,7 @@
         };
         end-of-line-diagnostics = "hint";
         inline-diagnostics.cursor-line = "warning";
+        bufferline = "multiple";
       };
     };
   };
